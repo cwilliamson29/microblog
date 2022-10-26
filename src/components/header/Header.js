60d86d81d17043bd6e3ef44eb8bcab6736e5bcb5
Chris Williamson <chriswill0629@gmail.com>
2022-10-26 16:52:24 -0400

    working on layout

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import "./header.css";
+
+export default function Header() {
+      return (
+            <div className="header">
+                  <div className="headerTitles">
+                        <span>React & Node</span>
+                        <span>Blog</span>
+                  </div>
+            </div>
+      );
+}
---
+++ src/components/pages/home/Home.js
@@ -0,0 +1,10 @@
+import Header from "../../header/Header";
+import "./home.css";
+
+export default function Home() {
+      return (
+            <div>
+                  <Header />
+            </div>
+      );
+}
