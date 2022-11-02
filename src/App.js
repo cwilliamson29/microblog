df0fefce49781cce9b7c5cb7431ff33d6e69ca5b
Chris Williamson <chriswill0629@gmail.com>
2022-11-02 16:55:25 -0400

    Working on layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./app.css";
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
+import Single from "./pages/single/Single";
 
 function App() {
       return (
             <>
                   <TopBar />
-                  <Home />
+                  <Single />
             </>
       );
 }
---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,5 @@
+import "./singlePost.css";
+
+export default function SinglePost() {
+      return <div className="single">SinglePost</div>;
+}
